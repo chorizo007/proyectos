034a223b11cb3f0e6404980c2a2562ae6697fbfd
chorizo007 <nicolashg2004@gmail.com>
2024-02-13 18:00:26 +0100

    drag-and-drop

---
+++ draganddrop/preguntas.php
@@ -0,0 +1,34 @@
+<?php
+$servidor = "192.168.3.181";
+$usuario = "nicolas";
+$contrasena = "1234";
+$base_datos = "pacofiesta";
+
+$conn = new mysqli($servidor, $usuario, $contrasena, $base_datos);
+
+if ($conn->connect_error) {
+    die("La conexión falló: " . $conn->connect_error);
+}
+
+$data = array();
+
+$sql = "SELECT * FROM tablapreguntas";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $cont = 0;
+    while ($row = $result->fetch_assoc()) {
+        $data['pregunta'] = $row['pregunta'];
+        $data['respuesta1'] = $row['respuesta1'];
+        $data['respuesta2'] = $row['respuesta2'];
+        $data['respuesta3'] = $row['respuesta3'];
+    }
+}
+
+
+header("Content-Type: application/json");
+echo json_encode($data);
+
+
+$conn->close();
