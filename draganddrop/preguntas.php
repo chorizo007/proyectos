03d0f4e4692124aaded5fa3d77273d0f7c83c375
chorizo007 <nicolashg2004@gmail.com>
2024-02-14 10:28:53 +0100

    Update preguntas.php

--- draganddrop/preguntas.php
+++ draganddrop/preguntas.php
@@ -2,7 +2,7 @@
 $servidor = "192.168.3.181";
 $usuario = "nicolas";
 $contrasena = "1234";
-$base_datos = "pacofiesta";
+$base_datos = "pruebaspreguntas";
 
 $conn = new mysqli($servidor, $usuario, $contrasena, $base_datos);
 
